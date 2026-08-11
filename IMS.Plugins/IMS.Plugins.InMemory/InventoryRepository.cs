3fb8922d93fbe44574a7886a012e97428798f301
AkshatParasher <bobbyparasher95@gmail.com>
2026-08-12 01:07:41 +0200

    Add project files.

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventoryRepository() 
+        {
+            _inventories = new List<Inventory>() 
+            {
+                new Inventory{ InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2},
+                new Inventory{ InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 10},
+                new Inventory{ InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 5},
+                new Inventory{ InventoryId = 4, InventoryName = "Bike Pedels", Quantity = 20, Price = 1}
+            };
+        }
+
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+            if(_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase))) 
+            {
+                return Task.CompletedTask;
+            }
+
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId + 1;
+
+            _inventories.Add(inventory);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return await Task.FromResult(_inventories);
+
+            return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
