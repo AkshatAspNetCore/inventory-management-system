2c3a1389799a0e606944604a4494174893a78125
AkshatParasher <bobbyparasher95@gmail.com>
2026-08-17 23:41:37 +0200

    Implemented viewinventorybyidusecase class

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -39,6 +39,11 @@ namespace IMS.Plugins.InMemory
             return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
 
+        public Task<Inventory> GetInventoryByIdAsync(int inventoryId)
+        {
+            return Task.FromResult(_inventories.First(x => x.InventoryId == inventoryId));
+        }
+
         public Task UpdateInventoryAsync(Inventory inventory)
         {
             if(_inventories.Any(x => x.InventoryId != inventory.InventoryId && x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
+builder.Services.AddTrasient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
 
 var app = builder.Build();
 
