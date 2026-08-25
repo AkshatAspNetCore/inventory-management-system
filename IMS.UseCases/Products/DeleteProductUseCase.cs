639cf147d85ef586f4e144460117e9587154f0b9
AkshatParasher <bobbyparasher95@gmail.com>
2026-08-25 18:08:07 +0200

    Implemented interactive channel with product delete use case

---
+++ IMS.UseCases/Products/DeleteProductUseCase.cs
@@ -0,0 +1,26 @@
+﻿using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class DeleteProductUseCase : IDeleteProductUseCase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductUseCase(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task ExecuteAsync(int ProductId)
+        {
+             await _productRepository.DeleteProductAsync(ProductId);
+        }
+    }
+}
