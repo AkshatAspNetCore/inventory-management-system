69c10b076c23376355094fcc81248bd737667890
AkshatParasher <bobbyparasher95@gmail.com>
2026-08-26 19:08:03 +0200

    Added add product ineractive SSR razor page plus edit product with all the use cases

---
+++ IMS.UseCases/Products/ViewProductByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Products
+{
+    public class ViewProductByIdUseCase : IViewProductByIdUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ViewProductByIdUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public async Task<Product> ExecuteAsync(int productId)
+        {
+            return await productRepository.GetProductByIdAsync(productId);
+        }
+    }
+}
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -2,6 +2,7 @@ using IMS.Plugins.InMemory;
 using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products;
 using IMS.UseCases.Products.Interfaces;
 using IMS.WebApp.Components;
 
@@ -22,6 +23,9 @@ builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 
 builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase>();
 builder.Services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+builder.Services.AddTransient<IEditProductUseCase, EditProductUseCase>();
+builder.Services.AddTransient<IViewProductByIdUseCase, ViewProductByIdUseCase>();
 
 var app = builder.Build();
 
