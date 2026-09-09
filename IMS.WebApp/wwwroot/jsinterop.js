83cee3a5946e9c00ee8bc0dbec9c0626498ae3f1
AkshatParasher <bobbyparasher95@gmail.com>
2026-09-09 14:56:37 +0200

    Implemented javascript interop feature to prevent form submission on enter click for produce product and purchase inventory

---
+++ IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,12 @@
+﻿function preventFormSubmission(formId) {
+
+    document.getElementById(formId).addEventListener('keydown', function (event) {
+
+        if (event.key == "Enter") {
+            event.preventDefault(); // prevent form submission
+            return false;  
+        }
+
+    });
+
+}
