fa9d3cd18db92a21bc0784062b48e81e19db758c
AkshatParasher <bobbyparasher95@gmail.com>
2026-09-16 17:46:38 +0200

    Added inventory and product efcore classes

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/ProductEFCoreRepository.cs
@@ -0,0 +1,86 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer
+{
+    public class ProductEFCoreRepository : IProductRepository
+    {
+        private readonly IDbContextFactory<IMSContext> contextFactory;
+
+        public ProductEFCoreRepository(IDbContextFactory<IMSContext> contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+        public async Task AddProductAsync(Product product)
+        {
+            using var db = contextFactory.CreateDbContext();
+            db.Products.Add(product);
+            FlagInventoryUnchanged(product, db);
+
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            using var db = contextFactory.CreateDbContext();
+            var product = await db.Products.FindAsync(productId);
+            if (product is null) return;
+
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int productId)
+        {
+            using var db = contextFactory.CreateDbContext();
+            var product = await db.Products.FindAsync(productId);
+
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByNameAsync(string name)
+        {
+            using var db = contextFactory.CreateDbContext();
+            if (string.IsNullOrWhiteSpace(name))
+                return await db.Products.ToListAsync();
+
+            return await db.Products.Where(x => x.ProductName.ToLower().IndexOf(name.ToLower()) >= 0).ToListAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            using var db = contextFactory.CreateDbContext();
+            var prod = await db.Products.FindAsync(product.ProductId);
+            if (prod is not null)
+            {
+                prod.ProductName = product.ProductName;
+                prod.Quantity = product.Quantity;
+                prod.Price = product.Price;
+                prod.ProductInventories = product.ProductInventories;
+                FlagInventoryUnchanged(product, db);
+            }
+
+            await db.SaveChangesAsync();
+        }
+
+        private void FlagInventoryUnchanged(Product product, IMSContext db) 
+        {
+            if (product.ProductInventories != null && product.ProductInventories.Count > 0)
+            {
+                foreach (var productInventory in product.ProductInventories)
+                {
+                    if (productInventory.Inventory != null)
+                    {
+                        db.Entry(productInventory.Inventory).State = EntityState.Unchanged;
+                    }
+                }
+            }
+        }
+    }
+}
